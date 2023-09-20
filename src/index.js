dd4ebd2523e52ed67d8a884231c57e2f65284d57
itsrusty <rusty3031@gmail.com>
2023-09-20 00:20:52 -0700

    deploy my rest api finally

--- src/index.js
+++ src/index.js
@@ -1,40 +1,50 @@
-import Fastify from "fastify"
-// import cors from "fastify-cors"
-import colors from "colors"
-import dotenv from "dotenv"
-import { connection } from "./config/database/connection.js"
-import { createUser, adminRoute } from "./routes/users.routes.js"
-// import cors from "@fastify/cors"
+import Fastify from "fastify";
+import colors from "colors";
+import dotenv from "dotenv";
+import { connection } from "./config/database/connection.js";
+import { createUser, adminRoute } from "./routes/users.routes.js";
 
-// TODO: enviroments 
-dotenv.config()
+// TODO: enviroments
+dotenv.config();
 
 // TODO: connection DB funciton
-connection()
+connection();
 
 // TODO: middlewares
-const PORT = process.env.PORT = 3000
+const PORT = (process.env.PORT = 3000);
 const fastify = Fastify({
-    logger: true
-})
+  logger: true,
+});
 
-// TODO: plugins 
-fastify.register(createUser)
-fastify.register(adminRoute)
-// await fastify.register(cors,{
-//     origin: true
-// })
+// TODO: plugins
+fastify.register(createUser);
+fastify.register(adminRoute);
+
+// TODO: config CORS method
+fastify.addHook("onRequest", (req, reply, done) => {
+  reply.header("Access-Control-Allow-Origin", "*");
+  reply.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  reply.header("Access-Control-Allow-Headers", "Content-Type");
+  reply.header("Access-Control-Allow-Credentials", "true");
+  reply.header("Access-Control-Max-Age", "86400");
+
+  if (req.method === "OPTIONS") {
+    reply.code(200).send();
+  } else {
+    done();
+  }
+});
 
 // TODO: Run server!
-const bootstrap = async() =>{
-    try {
-        await fastify.listen({
-            port: PORT
-        })
-        console.log(`listening on port ${PORT}`.green)
-    } catch (error) {
-        throw new Error(error)
-    }
-}
+const bootstrap = async () => {
+  try {
+    await fastify.listen({
+      port: PORT,
+    });
+    console.log(`listening on port ${PORT}`.green);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
 
-bootstrap()
+bootstrap();
