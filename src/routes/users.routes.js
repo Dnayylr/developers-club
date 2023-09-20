2ddd71a7334b21eb870c726688892af2026b6a01
itsrusty <rusty3031@gmail.com>
2023-09-19 19:03:21 -0700

    update routes

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,11 +1,15 @@
-import { createUserController } from "../controllers/users.controllers.js";
+import {
+  createUserController,
+  getUsers,
+} from "../controllers/users.controllers.js";
+import cacheInit from "../middlewares/cache.config.js";
 
-export const adminRoute = async (fastify, opts, done) =>{
-    await fastify.get("/admin")
-    done()
-}
+export const adminRoute = async (fastify, opts, done) => {
+  await fastify.get("/admin", cacheInit, getUsers);
+  done();
+};
 
-export const createUser = async (fastify, opts, done) =>{
-    await fastify.post("/", createUserController)
-    done()
-}
+export const createUser = async (fastify, opts, done) => {
+  await fastify.post("/", createUserController);
+  done();
+};
