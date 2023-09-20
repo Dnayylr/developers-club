2a61affe0122df1f708bb8421e2ed19fcf53eff0
itsrusty <rusty3031@gmail.com>
2023-09-19 18:48:01 -0700

    update controllers

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,5 +1,6 @@
 import userModel from "../models/user.model.js"
 
+// TODO: Create users
 export const createUserController = async (request, reply)=>{
     try {
         const { username, 
@@ -33,6 +34,22 @@ export const createUserController = async (request, reply)=>{
         }else{
             reply.send({ message: "funciona".bgMagenta, data: data})
         }
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+// TODO: Get Users
+export const getUsers = async (__request, reply)=>{
+    try {
+        const data = await userModel.find()
+        if(!data){
+            reply.send({ message: "error internal".red })
+        }else{
+            reply.send({ message: "funciona".bgBlue })
+            reply.send({ showData: data })
+        }
+
     } catch (error) {
         throw new Error(error)
     }
