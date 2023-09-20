f47f1e0c18e3d95717293b21252493308aa15db6
itsrusty <rusty3031@gmail.com>
2023-09-19 19:01:35 -0700

    add cache

---
+++ src/middlewares/cache.config.js
@@ -0,0 +1,14 @@
+import getExpeditiousCache from "express-expeditious"
+
+const config = {
+    namespace:"expresscache", // todo: el nombre o la key donde guardaremos en memoria
+    defaultTtl: "20 days",
+    statusCodeExpires: {
+        404: "5 minutes",
+        500: 0 
+    }
+}
+
+const cacheInit = getExpeditiousCache(config)
+
+export default cacheInit
